fab852e62a2b79b091eee0844680b8e0a79472f8
Msizi Moyo <146363264+Msimsi2005@users.noreply.github.com>
2026-02-02 22:30:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# This is the entry point for the Sibane Pre School Management System.
-from app import run_app  # Import the main app runner
+from app import run_app  
 
 if __name__ == "__main__":  # Check if script is run directly
     # Start the application loop defined in `app.run_app()`
-    run_app()  # Call the main app function
+    run_app() 
 
